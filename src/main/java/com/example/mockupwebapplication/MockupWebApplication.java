c756ea610f34a3cca0021619d51bc09007003852
IgorBoldysh <bushemi2001@gmail.com>
2021-06-17 20:26:32 +0300

    base spring boot initialized application

---
+++ src/main/java/com/example/mockupwebapplication/MockupWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockupwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockupWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockupWebApplication.class, args);
+	}
+
+}
