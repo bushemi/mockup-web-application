9cd1175796a0adb143470ba1e8bb92ead9b50563
IgorBoldysh <bushemi2001@gmail.com>
2021-06-20 09:06:01 +0300

    add ping endpoint

--- src/main/java/com/example/mockupwebapplication/controller/MainController.java
+++ src/main/java/com/example/mockupwebapplication/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.example.mockupwebapplication.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -10,5 +15,12 @@ public class MainController {
     @GetMapping(value = {"", "/", "/index", "/main"})
     public String mainPage() {
         return "mainPage";
+    }
+
+    @GetMapping(value = {"/ping"})
+    public ResponseEntity<Map<String, Object>> ping() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("some text", "answer");
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 }
